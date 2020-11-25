b4074445266457c1aa841dedc2cac5cbfc04a922
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2020-11-25 11:09:13 +0000

    Form to add items added

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
 from todo_app.data import session_items as si
@@ -10,6 +10,13 @@ app.config.from_object(Config)
 def index():
     items = si.get_items()
     return render_template('index.html', items =items)
+
+@app.route('/form', methods = ['GET','POST'])
+def form():
+    title = request.form ['title']
+    si.add_item(title)
+    items = si.get_items()
+    return render_template('index.html', items = items)
 
 if __name__ == '__main__':
     app.run()
