c4e2052904ddaa727b0dfc679aa4c26d6ef5aa79
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2020-11-30 18:07:52 +0000

    Attempt to add update status functionality

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 from todo_app.flask_config import Config
 from todo_app.data import session_items as si
@@ -17,7 +17,16 @@ def form():
     si.add_item(title)
     items = si.get_items()
     return render_template('index.html', items = items)
-    return redirect(url_for('index'))
+
+@app.route('/itemStatus', methods = ['GET','POST'])
+def itemStatus():
+    item_id = request.form ['itemID']
+    item_status = request.form ['itemStatus']
+    item = si.get_item(item_id)
+    updatedItem = {'id': item_id, 'title': item.title, 'status': item_status}
+    si.save_item(updatedItem)
+    items = si.get_items()
+    return render_template('index.html', items = items)
 
 if __name__ == '__main__':
     app.run()
