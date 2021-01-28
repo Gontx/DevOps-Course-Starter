7a46a5375bb02ea7f7fabcfca00147b48c053da9
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-01-28 12:52:10 +0000

    Added lists dictionary list

--- todo_app/requests_test.py
+++ todo_app/requests_test.py
@@ -7,6 +7,7 @@ base_url = 'https://trello.com/1/'
 id_board = 'sr8Gn9uE'
 payload ={ 'key' : api_key , 'token' : token }
 
+#Request board information
 r = requests.get(base_url + 'boards/' + id_board , params = payload)
 
 print(r.url)
@@ -16,16 +17,22 @@ r = r.json()
 print ('id for the board is ',r['id'])
 print ('Name of the board is ', r['name'])
 
+#Request lists on a board
 r=requests.get(base_url + 'boards/' + id_board + '/lists' , params = payload)
 print (r.status_code)
 r=r.json()
-#print (r)
+lists = []
+for key in r:
+    lists.append(dict({key['name']:key['id']}))
 
 print('Id for ', r[0]['name'], ' list is: ', r[0]['id'])
 id_todo = r[0]['id']
 
+#Request cards on a list
 r=requests.get(base_url + 'lists/' + id_todo + '/cards', params = payload)
 print (r.status_code)
 r=r.json()
 print(len(r))
-print (r[0]['name'])
+print ('Cards within list: ')
+for key in r:
+    print (key['name'])
