df766be3d3fab7cda8a3a7c492e4d09bd7276676
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-04-06 15:11:04 +0100

    Moved selenium tests to separate directory and created fixture for test board app

--- tests_selenium/test_selenium.py
+++ tests_selenium/test_selenium.py
@@ -1,8 +1,10 @@
 import requests
 import os
 import datetime as dt
+import pytest
 from dotenv import load_dotenv
 from todo_app.classes import Item
+from threading import Thread
 
 # Load .env variables
 load_dotenv()
@@ -20,12 +22,30 @@ def create_board(board_name):
     payload = { 'key' : api_key , 'token' : token , 'name' : board_name }
     r=requests.post(base_url+'boards/' , params = payload)
     r=r.json()
-    id_board = r['id']
-    return id_board
+    id_board = r['shortUrl']
+    id_board = id_board.rsplit('/', 1)[-1]
+    id_board_long = r['id']
+    return id_board,id_board_long
 
 def delete_board(id_board):
     # Deletes the speciafied board
     r=requests.delete(base_url+'boards/'+id_board, params = payload)
 
-id_new_board = create_board('TestBoard69')
-delete_board (id_new_board)
+@pytest.fixture(scope ='module')
+def app_with_temp_board():
+    # Create the new board & update the board id environment variable
+    (id_board,id_board_long) = create_board('SeleniumTestBoard')
+    os.environ['ID_BOARD'] = id_board
+
+    # Construct the new application
+    application = app.create_app()
+
+    # start the app in its own thread
+    thread = Thread(target=lambda:application.run(use_reloader=False))
+    thread.daemon = True
+    thread.start()
+    yield app
+
+    #Tear Down
+    thread.join(1)
+    delete_board (id_board_long)
