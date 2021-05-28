69cce69c5ebee4f8bb89f67b89e57c11a7bc7feb
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-05-28 12:43:40 +0100

    Modified tests to use hardcoded items instead of querying to trello

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,14 +5,20 @@ from dotenv import find_dotenv,load_dotenv
 import datetime as dt
 import pytest
 import json
+import pickle
 
 class TestUnit:
     @staticmethod
     def test_to_do_items():
         bOK = False
-        items = si.get_items()
-        item_view_model = ViewModel(items)
+
+        # Load hardcoded items
+        with open('items_pickle', 'rb') as f:
+            items_pickle = pickle.load(f)
+        item_view_model = ViewModel(items_pickle)
+
         to_do_items = item_view_model.to_do_items
+
         assert len(to_do_items) > 0
         for item in to_do_items:
             assert item.list == "To Do"
@@ -27,8 +33,12 @@ class TestUnit:
     @staticmethod
     def test_doing_items():
         bOK = False
-        items = si.get_items()
-        item_view_model = ViewModel(items)
+
+        # Load hardcoded items
+        with open('items_pickle', 'rb') as f:
+            items_pickle = pickle.load(f)
+        item_view_model = ViewModel(items_pickle)
+
         doing_items = item_view_model.doing_items
         assert len(doing_items) > 0
         for item in doing_items:
@@ -37,8 +47,12 @@ class TestUnit:
     @staticmethod
     def test_done_items():
         bOK = False
-        items = si.get_items()
-        item_view_model = ViewModel(items)
+        
+        # Load hardcoded items
+        with open('items_pickle', 'rb') as f:
+            items_pickle = pickle.load(f)
+        item_view_model = ViewModel(items_pickle)
+
         done_items = item_view_model.done_items
         assert len(done_items) > 0
         for item in done_items:
@@ -49,8 +63,12 @@ class TestUnit:
         bOK = False
         # Get today
         today = dt.date.today()
-        items = si.get_items()
-        item_view_model = ViewModel(items)
+
+        # Load hardcoded items
+        with open('items_pickle', 'rb') as f:
+            items_pickle = pickle.load(f)
+        item_view_model = ViewModel(items_pickle)
+
         recent_items = item_view_model.recent_done_items
         for item in recent_items:
             if item.date_last_activity.date() == today:
@@ -65,8 +83,12 @@ class TestUnit:
         bOK = False
         # Get today
         today = dt.date.today()
-        items = si.get_items()
-        item_view_model = ViewModel(items)
+        
+        # Load hardcoded items
+        with open('items_pickle', 'rb') as f:
+            items_pickle = pickle.load(f)
+        item_view_model = ViewModel(items_pickle)
+        
         old_done_items = item_view_model.older_done_items
         for item in old_done_items:
             if item.date_last_activity.date() < today:
