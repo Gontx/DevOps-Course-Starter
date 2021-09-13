2fca0dbc360fa1c61e3f23d4ae0b76f86d15b47c
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-09-13 20:47:01 +0100

    added heroku script and deployment stage to travis file

---
+++ herokuscript.sh
@@ -0,0 +1,11 @@
+# Get latest docker image from Docker Hub
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
+docker pull gontx/todo-app:latest
+
+# Tag it for Heroku
+
+docker tag gontx/todo-app:latest registry.heroku.com/gont-todo-app/web
+
+# Push it to heroku registry
+
+docker push registry.heroku.com/gontx-todo-app/web
