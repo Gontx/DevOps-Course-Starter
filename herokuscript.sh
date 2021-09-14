3c4b78ecf0c2781f39562bf7bf707f8680d7b3b9
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-09-14 18:51:56 +0100

    another iteration

--- herokuscript.sh
+++ herokuscript.sh
@@ -3,11 +3,10 @@ echo "Login to docker & heroku"
 docker login --username=_ --password=${HEROKU_API_KEY} registry.heroku.com
 # Tag it for Heroku
 echo "Tag image for heroku"
-docker pull gontx/todo-app:latest
 docker tag gontx/todo-app:latest registry.heroku.com/gontx-todo-app/web:latest
 # Push it to heroku registry
 echo "Push image to heroku"
 docker push registry.heroku.com/gontx-todo-app/web:latest
 echo "Release web"
-heroku container:release web
+heroku container:release web -a gontx-todo-app
 echo "You made it"
