469084a97cb77326da7db726e3314d337b9b67ae
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-09-14 18:53:01 +0100

    fixed typo

--- herokuscript.sh
+++ herokuscript.sh
@@ -3,10 +3,10 @@ echo "Login to docker & heroku"
 docker login --username=_ --password=${HEROKU_API_KEY} registry.heroku.com
 # Tag it for Heroku
 echo "Tag image for heroku"
-docker tag gontx/todo-app:latest registry.heroku.com/gontx-todo-app/web:latest
+docker tag gontx/todo-app:latest registry.heroku.com/gontx-todo-app/web
 # Push it to heroku registry
 echo "Push image to heroku"
-docker push registry.heroku.com/gontx-todo-app/web:latest
+docker push registry.heroku.com/gontx-todo-app/web
 echo "Release web"
 heroku container:release web -a gontx-todo-app
 echo "You made it"
