094ecd9b586ffe1f126fe09b3171c6e51758c7a1
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-12-02 17:33:04 +0000

    moved to single envar for cosmos connection string

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -18,8 +18,10 @@ def connect_mongo():
     cosmos_primary_master_key = os.getenv('PRIMARY_MASTER_KEY')
     cosmos_url = os.getenv('COSMOS_URL')
     cosmos_port = os.getenv('COSMOS_PORT')
+    cosmos_connection_string = os.getenv('COSMOS_CONNECTION_STRING')
     # Connect to CosmosDB using mongo api:
-    client = pymongo.MongoClient(f'mongodb://{str(cosmos_database_name)}:{str(cosmos_primary_master_key)}@{str(cosmos_database_name)}.{str(cosmos_url)}:{str(cosmos_port)}/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@{str(cosmos_database_name)}@')
+    client = pymongo.MongoClient(cosmos_connection_string)
+    #client = pymongo.MongoClient(f'mongodb://{str(cosmos_database_name)}:{str(cosmos_primary_master_key)}@{str(cosmos_database_name)}.{str(cosmos_url)}:{str(cosmos_port)}/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@{str(cosmos_database_name)}@')
     global db
     db = client.board
 
