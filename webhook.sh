d104ad0584ad849077ef5d0b245b0be7e3e5677c
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-12-08 15:59:04 +0000

    updated webhook script and travis file to trigger terraform

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,4 @@
 # Run Azure webhook
 echo "About to run Azure webhook"
-#curl -dH -X POST "$WEBHOOK_URL"
-terraform apply -var client_id=$CLIENT_ID -var client_secret=$CLIENT_SECRET -var secret_key=$SECRET_KEY
 curl -dH -X POST "$(terraform output webhook_url)"
 echo "Success"
