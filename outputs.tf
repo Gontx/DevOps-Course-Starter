6b45bcb12b7e147611b69cee4a57250e2fa5953f
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-12-09 19:43:33 +0000

    typo in webhook.sh and iteration to store properly the webhook url

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "webapp_url" {
-    value = "hhtps://${azurerm_app_service.my_app_service_container.default_site_hostname}"
+    value = "https://${azurerm_app_service.my_app_service_container.default_site_hostname}"
 }
 
 output "webhook_url" {
-    value ="https://${azurerm_app_service.my_app_service_container.site_credential[0].username}:${azurerm_app_service.my_app_service_container.site_credential[0].password}@${azurerm_app_service.my_app_service_container.name}.scm.azurewebsites.net/docker/hook"
+    value = "https://${azurerm_app_service.my_app_service_container.site_credential[0].username}:${azurerm_app_service.my_app_service_container.site_credential[0].password}@${azurerm_app_service.my_app_service_container.name}.scm.azurewebsites.net/docker/hook"
     sensitive = true
 }
