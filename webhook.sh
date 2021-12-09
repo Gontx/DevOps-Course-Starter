a1fd91aa7b78afc734fb84fc94a11db6fbfdd339
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-12-09 21:55:04 +0000

    removing terraform init from scritp

--- webhook.sh
+++ webhook.sh
@@ -1,6 +1,6 @@
 # Run Azure webhook
 echo "About to run Azure webhook"
-terraform init
+
 webhook_url=$(terraform output webhook_url)
 webhook_url_stripped=$(echo $webhook_url | tr -d '"')
 curl --globoff -dH -X POST $webhook_url_stripped
