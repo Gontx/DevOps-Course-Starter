1d5449b549503004afc54f6c038d3572d6e8c74b
Gonzalo Otegui <gonzalo.oteguifernan@mclaren.com>
2021-12-09 19:17:55 +0000

    rolled back sensitive output

--- webhook.sh
+++ webhook.sh
@@ -1,4 +1,4 @@
 # Run Azure webhook
 echo "About to run Azure webhook"
-curl --globoff -dH -X POST '$(terraform output -raw webhook_url)'
+curl --globoff -dH -X POST '$(terraform output webhook_url)'
 echo "Success"
